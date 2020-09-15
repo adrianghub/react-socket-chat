7e1e7fb91b64c2ba44db10e164843375f3ad3e72
Adrian <adrian_zinko@op.pl>
2020-09-15 15:18:41 +0200

    feat: provide styles for component

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Input } from '@material-ui/core';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+
+import logoImage from './logo.png';
+import classes from './Join.module.css';
 
 const Join = () => {
+  const [ name, setName ] = useState('')
+  const [ room, setRoom ] = useState('')
+
   return (
     <>
-    <h1>Join</h1> 
+      <div className={classes.Login}>
+        <div className={classes.LoginContainer}>
+          <img src={logoImage} alt="Logo describes author's creativity" />
+          <h1>
+            Join
+          </h1>
+          <div><Input type="text" placeholder="Name"/></div>
+          <div><Input type="text" placeholder="Room"/></div>
+          <Button color="primary" variant="contained"><PlayCircleOutlineIcon /></Button>
+        </div>
+      </div>
     </>
   );
 };
 
-export default Join;
+export default Join
