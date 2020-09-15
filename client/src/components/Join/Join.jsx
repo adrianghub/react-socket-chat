2202c5d1e12e26548b51221ae5a4c48aa8aec2ab
Adrian <adrian_zinko@op.pl>
2020-09-15 19:50:20 +0200

    feat: add finish touch for join

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,30 +1,72 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Input } from '@material-ui/core';
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Input } from "@material-ui/core";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 
-import logoImage from './logo.png';
-import classes from './Join.module.css';
+import logoImage from "../../images/logo.png";
+import classes from "./Join.module.css";
 
 const Join = () => {
-  const [ name, setName ] = useState('')
-  const [ room, setRoom ] = useState('')
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleRoomChange = (e) => {
+    setRoom(e.target.value);
+  };
+
+  const handleJoinSubmit = (e) => {
+    return !name || !room ? e.preventDefault() : null;
+  };
 
   return (
     <>
       <div className={classes.Login}>
         <div className={classes.LoginContainer}>
           <img src={logoImage} alt="Logo describes author's creativity" />
-          <h1>
-            Join
-          </h1>
-          <div><Input type="text" placeholder="Name"/></div>
-          <div><Input type="text" placeholder="Room"/></div>
-          <Button color="primary" variant="contained"><PlayCircleOutlineIcon /></Button>
+          <h1>Join Room</h1>
+          <div>
+            <Input
+              type="text"
+              placeholder="Type chat name..."
+              onChange={(e) => handleNameChange(e)}
+              classes={{
+                root: classes.InputJoin,
+              }}
+            />
+          </div>
+          <div>
+            <Input
+              type="text"
+              placeholder="Type chat room..."
+              onChange={(e) => handleRoomChange(e)}
+              classes={{
+                root: classes.InputJoin,
+              }}
+            />
+          </div>
+          <Link
+            onClick={(e) => handleJoinSubmit(e)}
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <Button
+              fullWidth
+              color="primary"
+              variant="contained"
+              classes={{
+                root: classes.ButtonSubmit,
+              }}
+            >
+              <PlayCircleOutlineIcon />
+            </Button>
+          </Link>
         </div>
       </div>
     </>
   );
 };
 
-export default Join
+export default Join;
